10459a9f598966d641fee3ec65485f64c21e23a6
przemyslawstudzinski <przemyslawstudzinski1993@gmail.com>
2016-07-29 01:41:08 +0200

    Add MVVM Structure

---
+++ Inzynierka/ControlDiet/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationToSupportAndControlDiet.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            WelcomeMessage = "Hi, it's main page";
+        }
+        private string _welcomeMessage;
+        public string WelcomeMessage
+        {
+            get
+            {
+                return _welcomeMessage;
+            }
+            set
+            {
+                Set(ref _welcomeMessage, value);
+            }
+        }
+    }
+}
