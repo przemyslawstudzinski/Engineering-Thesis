864fb5ac92973297397dd475bb412d926c53b1a6
przemyslawstudzinski <przemyslawstudzinski1993@gmail.com>
2016-07-28 23:49:32 +0200

    Change name of namespace and name of aplication

--- Inzynierka/ControlDiet/Properties/AssemblyInfo.cs
+++ Inzynierka/ControlDiet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Inzynierka")]
+[assembly: AssemblyTitle("ControlDiet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Inzynierka")]
+[assembly: AssemblyProduct("ControlDiet")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
