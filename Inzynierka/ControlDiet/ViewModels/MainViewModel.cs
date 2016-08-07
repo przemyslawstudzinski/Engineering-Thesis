8f8e912187adeecba4abbd5b6653d5ca1e3621b9
przemyslawstudzinski <przemyslawstudzinski1993@gmail.com>
2016-08-07 21:39:46 +0200

    add database

--- Inzynierka/ControlDiet/ViewModels/MainViewModel.cs
+++ Inzynierka/ControlDiet/ViewModels/MainViewModel.cs
@@ -14,6 +14,7 @@ namespace ApplicationToSupportAndControlDiet.ViewModels
         public MainViewModel()
         {
             WelcomeMessage = "Hi, it's main page";
+            DatabaseConnection.ConnectToSqliteDatabase();
         }
         private string _welcomeMessage;
         public string WelcomeMessage
