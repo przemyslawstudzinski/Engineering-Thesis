cc83876011d961383e0c972c664128a43cfbba18
Kichachi <kichachiminato@gmail.com>
2016-09-04 21:36:29 +0200

    Product Creator

--- Inzynierka/ControlDiet/MainPage.xaml.cs
+++ Inzynierka/ControlDiet/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ApplicationToSupportAndControlDiet.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,6 +27,7 @@ namespace ApplicationToSupportAndControlDiet
         public MainPage()
         {
             this.InitializeComponent();
+            DatabaseConnection.ConnectToSqliteDatabase();
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
