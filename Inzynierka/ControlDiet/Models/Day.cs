0605ee158931149e8e81aba3e3ef9e8eb25950d1
przemyslawstudzinski <przemyslawstudzinski1993@gmail.com>
2016-10-09 22:24:53 +0200

    calculate nutrition in day

--- Inzynierka/ControlDiet/Models/Day.cs
+++ Inzynierka/ControlDiet/Models/Day.cs
@@ -24,5 +24,95 @@ namespace ApplicationToSupportAndControlDiet.Models
 
         [OneToMany]
         public List<Meal> MealsInDay { set; get; }
+
+        public float energy;
+
+        public float Energy
+        {
+            get
+            {
+                energy = 0;
+                foreach (Meal element in MealsInDay)
+                {
+                    energy += element.Energy;
+                }
+                return energy;
+            }
+        }
+
+        public float protein;
+
+        public float Protein
+        {
+            get
+            {
+                protein = 0;
+                foreach (Meal element in MealsInDay)
+                {
+                    protein += element.Protein;
+                }
+                return protein;
+            }
+        }
+
+        public float carbohydrate;
+
+        public float Carbohydrate
+        {
+            get
+            {
+                carbohydrate = 0;
+                foreach (Meal element in MealsInDay)
+                {
+                    carbohydrate += element.Carbohydrate;
+                }
+                return carbohydrate;
+            }
+        }
+
+        public float fat;
+
+        public float Fat
+        {
+            get
+            {
+                fat = 0;
+                foreach (Meal element in MealsInDay)
+                {
+                    fat += element.Fat;
+                }
+                return fat;
+            }
+        }
+
+        public float sugar;
+
+        public float Sugar
+        {
+            get
+            {
+                sugar = 0;
+                foreach (Meal element in MealsInDay)
+                {
+                    sugar += element.Sugar;
+                }
+                return sugar;
+            }
+        }
+
+        public float fiber;
+
+        public float Fiber
+        {
+            get
+            {
+                fiber = 0;
+                foreach (Meal element in MealsInDay)
+                {
+                    fiber += element.Fiber;
+                }
+                return fiber;
+            }
+        }
     }
 }
